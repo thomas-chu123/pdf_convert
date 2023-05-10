88b1f3d8c4545cff62fca29a8fbe7fb9fb97e0e6
chu thomas <chu_liang_han@hotmail.com>
2023-05-10 10:51:41 +0800

    5/10

--- convert.py
+++ convert.py
@@ -3,7 +3,7 @@ import re
 import PyPDF2
 import openpyxl
 
-title_list = ['title', 'Case ID', 'Test type', 'Test case coverage', ' Preconditions', 'Test Steps', 'Expected Results']
+title_list = ['Title', 'Case ID', 'Test type', 'Test case coverage', 'Preconditions', 'Test Steps', 'Expected Results']
 
 # Change directory to the folder containing PDF files
 # os.chdir('.')
@@ -50,29 +50,36 @@ for filename in file_data:
         col = col + 1
 
     for line in file_data[filename]:
-        if re.match('\d.\d.\d', line) and "....." not in line:
+        if re.match(r'[1-9]\.\d', line) and "....." not in line:
             test_case = {}
             if test_case != None:
                 test_case_block.append(test_case)
             start_expectations = False
-            test_case['title'] = line
+            test_case['Title'] = line
         if "Case ID" in line:
             start_case = True
+            continue
         if "Test type" in line:
             start_case = False
             start_test_type = True
+            continue
         if "Test case coverage" in line:
             start_test_type = False
             start_test_coverage = True
+            continue
         if "Preconditions" in line:
             start_test_coverage = False
             start_preconditions = True
+            continue
         if "Test Steps" in line:
             start_preconditions = False
             start_test_steps = True
+            continue
         if "Expected Results" in line:
             start_test_steps = False
             start_expectations = True
+            continue
+
         if start_case == True:
             test_case['Case ID'] = line
         if start_test_type == True:
@@ -81,22 +88,22 @@ for filename in file_data:
             test_case['Test case coverage'] = line
         if start_preconditions == True:
             if 'Preconditions' in test_case:
-                test_case['Preconditions'] = test_case['Preconditions'] + line
+                test_case['Preconditions'] = test_case['Preconditions'] + "\n" + line
             else:
                 test_case['Preconditions'] = line
         if start_test_steps == True:
             if 'Test Steps' in test_case:
-                test_case['Test Steps'] = test_case['Test Steps'] + line
+                test_case['Test Steps'] = test_case['Test Steps'] + "\n" + line
             else:
                 test_case['Test Steps'] = line
         if start_expectations == True:
-            if 'Expectations' in test_case:
-                test_case['Expectations'] = test_case['Expectations'] + line
+            if 'Expected Results' in test_case:
+                test_case['Expected Results'] = test_case['Expected Results'] + "\n" + line
             else:
-                test_case['Expectations'] = line
-        previous_line = line
+                test_case['Expected Results'] = line
+        # previous_line = line
 
-    row = 1
+    row = 2
     col = 1
     for test_case in test_case_block:
         col = 1
